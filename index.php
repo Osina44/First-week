d6b2bd871bf4c9b053a52dd67a4079b7b9a4c20b
Osina44 <13871@ait.nsw.edu.au>
2025-02-20 00:16:51 +0000

    updated

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
 require_once 'vendor/autoload.php';
+//classes used in this page
+use Osina\Classproject\App;
+
+//Create app from App class
+$app = new App();
+$site_name = $app-> site_name;
 //create data variables
 $page_title = "Class Project Home Page";
 $greeting = "Welcome to my website";
@@ -9,5 +15,11 @@ $loader = new \Twig\Loader\FilesystemLoader('Templates');
 $twig = new \Twig\Environment($loader);
 $template = $twig -> load('page.twig' );
 //render the output
-echo $template -> render( [ 'title' => $page_title, 'greeting' => $greeting] );
+echo $template -> render( [
+     'title' => $page_title, 
+     'greeting' => $greeting,
+     'website_name' => $site_name,
+     ] )
+     
+
 ?>
